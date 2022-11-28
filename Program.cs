2ca3e21a743ed84f7a0ebf58cbff1649e0f10b76
Jasmine Carrasco <jasminecarrasco@Jasmines-MacBook-Air.local>
2022-11-28 15:48:06 -0700

    finshed

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Random r = new Random();
+string[] Firstnames = new string[10] {"Emma", "Jasmine", "Rose", "Luna", "Aron", "Vicente", "Ed", "Ron", "Kylian", "Andy"};
+string[]Middlenames = new string [10] {" Ann", " Lyn", " Franci", " Irene", " Mae", " Jane", " Marie"," Eve", " Kay", " Dawn"};
+string[]Lastnames = new string [10] {" Carrasco", " Trejo", " Jimenez"," Larsen", " Smith", " Lee", " Day",  " Lune", " Clair", " Sol", };
+for (int x = 0; x<Firstnames.Length; x++)
+{
+   for (int y = 0; y < Middlenames.Length; y++)
+   {
+
+      for (int z = 0; z < Lastnames.Length;   z++)
+      {
+
+       string Peoplesnames = Firstnames[x] + Middlenames[y] + Lastnames[z];
+        
+
+        Console.WriteLine($" Name: {Peoplesnames}  ");
+
+      }
+
+
+
+
+
+
+   }
+}
